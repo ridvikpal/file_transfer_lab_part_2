a315e7a53499a16f203c5e2864d8cb6c68ae888f
Ridvik Pal <ridvik.pal@mail.utoronto.ca>
2024-02-07 19:49:34 -0500

    working server.c

--- server.c
+++ server.c
@@ -50,112 +50,118 @@ int main(int argc, char* argv[]){
         socklen_t clientAddressSize = sizeof(clientSockAddrIn);
         memset(&clientSockAddrIn, 0, sizeof(clientSockAddrIn));
 
-        // setup the connection first
-        ssize_t message_length = recvfrom(udpSocket, (char*)message, MESSAGE_SIZE, 0,
-                                          (struct sockaddr*)&clientSockAddrIn, &clientAddressSize);
-
-        if (message_length < 0){
-            printf("Failed to receive message.\n");
-            exit(1);
-        }
-
-        // null terminate the string so we can print via printf() easily
-        message[message_length] = '\0';
-
-        printf("Received message: %s\n", message);
-
-        // setup the response message
-        if (strcmp(message, "ftp") == 0){
-            strcpy(message, "yes");
-        }else{
-            strcpy(message, "no");
-        }
-
-        printf("Sending message: %s\n", message);
-
-        // send the response to the client
-        if (sendto(udpSocket, message, strlen(message), 0, (struct sockaddr*)&clientSockAddrIn,
-                   clientAddressSize)< 0) {
-            printf("Failed to send message.\n");
-            exit(1);
-        }
-
-        while (1){
-            // receive incoming packet via UDP connection
-            ssize_t message_length = recvfrom(udpSocket, message, MESSAGE_SIZE, 0,
+        while(1){
+            // setup the connection first
+            ssize_t message_length = recvfrom(udpSocket, (char*)message, MESSAGE_SIZE, 0,
                                               (struct sockaddr*)&clientSockAddrIn, &clientAddressSize);
 
             if (message_length < 0){
                 printf("Failed to receive message.\n");
-                continue;
+                exit(1);
             }
 
-            struct packet incomingPacket;
+            // null terminate the string so we can print via printf() easily
+            message[message_length] = '\0';
 
+            printf("Received message: %s\n", message);
 
-            // allocate space for the incoming packet filename
-            incomingPacket.filename = malloc(256);
-            if (!(incomingPacket.filename)){
-                printf("Malloc for incoming packet filename failed");
-                continue;
+            // setup the response message
+            if (strcmp(message, "ftp") == 0){
+                strcpy(message, "yes");
+            }else{
+                strcpy(message, "no");
             }
 
-            // store all data that is a regular string in our incomingPacket variable
-            sscanf(message, "%u:%u:%u:%255[^:]:", &incomingPacket.total_frag, &incomingPacket.frag_no,
-                   &incomingPacket.size, incomingPacket.filename);
-            // calculate the header size
+            printf("Sending message: %s\n", message);
+
+            // send the response to the client
+            if (sendto(udpSocket, message, strlen(message), 0, (struct sockaddr*)&clientSockAddrIn,
+                       clientAddressSize)< 0) {
+                printf("Failed to send message.\n");
+                exit(1);
+            }
+
+            int done = 0;
+
+            while (done == 0){
+                // receive incoming packet via UDP connection
+                ssize_t message_length = recvfrom(udpSocket, message, MESSAGE_SIZE, 0,
+                                                  (struct sockaddr*)&clientSockAddrIn, &clientAddressSize);
+
+                if (message_length < 0){
+                    printf("Failed to receive message.\n");
+                    continue;
+                }
+
+                struct packet incomingPacket;
+
+                // allocate space for the incoming packet filename
+                incomingPacket.filename = malloc(256);
+                if (!(incomingPacket.filename)){
+                    printf("Malloc for incoming packet filename failed");
+                    continue;
+                }
+
+                // store all data that is a regular string in our incomingPacket variable
+                // store the header data
+                sscanf(message, "%u:%u:%u:%255[^:]:", &incomingPacket.total_frag, &incomingPacket.frag_no,
+                       &incomingPacket.size, incomingPacket.filename);
+                // calculate the header size
 //            int header_size = sizeof(incomingPacket.total_frag) + sizeof(incomingPacket.frag_no)
 //                    + sizeof(incomingPacket.size) + sizeof(incomingPacket.filename);
 
-            // calculate the header size using snprintf()
-            char tempString[MESSAGE_SIZE];
-            int header_size = snprintf(tempString, sizeof(tempString), "%u:%u:%u:%s:", incomingPacket.total_frag,
-                                       incomingPacket.frag_no, incomingPacket.size, incomingPacket.filename);
+                // calculate the header size using snprintf()
+                char tempString[MESSAGE_SIZE];
+                int header_size = snprintf(tempString, sizeof(tempString), "%u:%u:%u:%s:", incomingPacket.total_frag,
+                                           incomingPacket.frag_no, incomingPacket.size, incomingPacket.filename);
 
-            // copy the filedata to the incomingPacket
-            memcpy(incomingPacket.filedata, message + header_size, incomingPacket.size);
-            printf("Got here\n");
+                // copy the filedata to the incomingPacket
+                memcpy(incomingPacket.filedata, message + header_size, incomingPacket.size);
 
-            //if the packet is received, open file stream
-            // we keep the file stream open starting from when first fragment comes until last fragment comes
-            // only open the file stream once
-            if (incomingPacket.frag_no == 1){
-                if(file){ // if the file pointer exists, then close it
-                    fclose(file);
+                //if the packet is received, open file stream
+                // we keep the file stream open starting from when first fragment comes until last fragment comes
+                // only open the file stream once
+                if (incomingPacket.frag_no == 1){
+                    if(file){ // if the file pointer exists, then close it
+                        fclose(file);
+                    }
+                    file = fopen(incomingPacket.filename, "wb");
+                    if (!file){ // if the file pointer is still null
+                        printf("Opening the file failed");
+                        free(incomingPacket.filename);
+                        continue;
+                    }
                 }
-                file = fopen(incomingPacket.filename, "wb");
-                if (!file){ // if the file pointer is still null
-                    printf("Opening the file failed");
-                    free(incomingPacket.filename);
+
+                // write the data to the local machine
+                size_t bytes_written = fwrite(incomingPacket.filedata, 1, incomingPacket.size, file);
+                if (bytes_written != incomingPacket.size){
+                    printf("Unable to write to local machine");
                     continue;
                 }
+
+                printf("Recieved fragment %u of %u\n", incomingPacket.frag_no, incomingPacket.total_frag);
+
+                // if all fragments are received, only then close the file stream
+                if (incomingPacket.frag_no == incomingPacket.total_frag){
+                    done = 1;
+                    fclose(file);
+                    file = NULL;
+                }
+
+                char response[] = "ACK";
+
+                // send the response to the client
+                if (sendto(udpSocket, response, strlen(response), 0, (struct sockaddr*)&clientSockAddrIn,
+                           clientAddressSize) < 0) {
+                    printf("Failed to send ACK response.\n");
+                    continue;
+                }
+
+                free(incomingPacket.filename);
             }
-
-            // write the data to the local machine
-            size_t bytes_written = fwrite(incomingPacket.filedata, 1, incomingPacket.size, file);
-            if (bytes_written != incomingPacket.size){
-                printf("Unable to write to local machine");
-                continue;
-            }
-
-            printf("Recieved fragment %u of %u\n", incomingPacket.frag_no, incomingPacket.total_frag);
-
-            // if all fragments are received, only then close the file stream
-            if (incomingPacket.frag_no == incomingPacket.total_frag){
-                fclose(file);
-                file = NULL;
-            }
-
-            char response[] = "ACK";
-
-            // send the response to the client
-            if (sendto(udpSocket, response, strlen(response), 0, (struct sockaddr*)&clientSockAddrIn,
-                    clientAddressSize)< 0) {
-                printf("Failed to send ACK response.\n");
-                continue;
-            }
-            free(incomingPacket.filename);
         }
+
 
         // close the file descriptor after it has been used
         close(udpSocket);
