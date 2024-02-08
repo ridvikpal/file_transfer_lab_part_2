543838ed3a1cfb5105d099767939dae22e8b184c
cuikalvi <kalvin.cui@mail.utoronto.ca>
2024-02-07 19:23:13 -0500

    Implemented packets and fragmentation for deliver.c

--- deliver.c
+++ deliver.c
@@ -6,6 +6,18 @@
 #include <arpa/inet.h>
 #include <time.h>
 
+#define BUFFER_SIZE 1024
+#define MAX_PACKET_SIZE 1000
+
+struct packet {
+    unsigned int total_frag;
+    unsigned int frag_no;
+    unsigned int size;
+    char* filename;
+    char filedata[1000];
+};
+
+
 int main(int argc,char *argv[])
 {
     //Check to see if there are the correct number of arguments
@@ -14,9 +26,9 @@ int main(int argc,char *argv[])
         return -1;
     }
     char *str = NULL;
-    str = (char *) malloc(1024);
+    str = (char *) malloc(BUFFER_SIZE);
     printf ("Please input a message as follows: ftp <file name>\n");
-    fgets(str, 1024, stdin);
+    fgets(str, BUFFER_SIZE, stdin);
 
     if (strncmp (str, "ftp ", 4) != 0){
         printf ("Command must start with \"ftp\" \n");
@@ -58,14 +70,14 @@ int main(int argc,char *argv[])
     //Make the socket
     int sockfd;
     if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0))<0){
-        perror ("Error making the socket\n");
+        printf ("Error making the socket\n");
         return -1;
     }
     clock_t start_time = clock();
 
     //Send "ftp"
     if (sendto(sockfd, "ftp", strlen("ftp"), MSG_CONFIRM, (struct sockaddr *) &server_address, sizeof (server_address))<0){
-        perror ("Sending ftp failed\n");
+        printf ("Sending ftp failed\n");
         close(sockfd);
         return -1;
     }
@@ -74,19 +86,19 @@ int main(int argc,char *argv[])
     //Receive message from the server
     ssize_t msg_len;
     socklen_t client_address_size = sizeof(server_address);
-    char buffer [1024];
-    msg_len = recvfrom(sockfd, (char*) buffer, 1024, 0, (struct sockaddr*) &server_address, &client_address_size);
+    char buffer [BUFFER_SIZE];
+    msg_len = recvfrom(sockfd, (char*) buffer, BUFFER_SIZE, 0, (struct sockaddr*) &server_address, &client_address_size);
 
 
     if (msg_len == -1){
-        perror("recvfrom failed");
+        printf("recvfrom failed");
         close(sockfd);
         return -1;
     }
 
     clock_t end_time = clock();
-    double time_elapsed = ((double)(end_time - start_time))/ CLOCKS_PER_SEC;
-    printf("%fs have elaspsed\n", time_elapsed);
+    double rtt = ((double)(end_time - start_time))/ CLOCKS_PER_SEC;
+    printf("Round Trip Time (RTT): %fs\n", rtt);
 
     buffer[msg_len] = '\0';
 
@@ -96,6 +108,73 @@ int main(int argc,char *argv[])
         printf ("Exiting");
         return -1;
     }
+
+    //Try to open the file
+    FILE *file = fopen (file_name, "rb");
+    if (!file){
+        printf("Error opening file with name: %s\n", file_name);
+        return -1;
+    }
+
+    //Find the size of the file
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    //Find total number of fragments
+    int total_fragment = (file_size / MAX_PACKET_SIZE) + 1;
+
+    struct timeval timeout;
+    timeout.tv_sec = 2; //2 second timeout
+    timeout.tv_usec = 0;
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0){
+        printf ("Error in setsockopt\n");
+        return -1;
+    }
+    size_t bytes_read;
+
+    //Create the packet
+    struct packet outgoing_packet;
+
+    while ((bytes_read = fread(outgoing_packet.filedata, 1, MAX_PACKET_SIZE, file)) > 0) {
+        int ack = 0;
+
+        outgoing_packet.filename = malloc(strlen(file_name)+1);
+        if (!outgoing_packet.filename){
+            printf("Error in allocating memory in packet\n");
+            return -1;
+        }
+        strcpy(outgoing_packet.filename, file_name);
+        outgoing_packet.size = bytes_read;
+        int header_size = sprintf(buffer, "%u:%u:%u:%s:", outgoing_packet.total_frag, outgoing_packet.frag_no, outgoing_packet.size,
+                              outgoing_packet.filename);
+        memcpy(buffer + header_size, outgoing_packet.filedata, outgoing_packet.size);
+
+        while (!ack){
+            if (sendto(sockfd, buffer, BUFFER_SIZE, MSG_CONFIRM, (struct sockaddr *) &server_address, sizeof (server_address))<0){
+                printf ("Sending packet failed\n");
+                fclose(file);
+                close(sockfd);
+                return -1;
+            }
+            msg_len = recvfrom(sockfd, (char*) buffer, BUFFER_SIZE, 0, (struct sockaddr*) &server_address, &client_address_size);
+            if (msg_len == -1){
+                printf("recvfrom failed");
+                close(sockfd);
+                return -1;
+            }
+            if (msg_len > 0 && strncmp(buffer, "ACK", 3) == 0){
+                ack = 1;
+                printf("ACK received for fragment number %u\n", outgoing_packet.frag_no);
+            }else {
+                printf ("Waiting for ACK for fragment number %u\n", outgoing_packet.frag_no);
+            }
+        }
+        free(outgoing_packet.filename);
+        outgoing_packet.frag_no++;
+    }
+    fclose(file);
     free(str);
     close (sockfd);
     return 0;
